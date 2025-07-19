c3e75817f4e5bfc2929840456dbe729d40ce5e68
HawiGirma <hawigirmamegersag@gmail.com>
2025-07-20 00:48:16 +0300

    post card update

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,11 +1,14 @@
 import { PostProps } from "@/interfaces/PostProps";
 
-export default function PostCard({ id, title, body }: PostProps) {
+export default function PostCard({ id, title, body, userId }: PostProps) {
   return (
     <div className="bg-white p-4 rounded-xl shadow hover:shadow-lg transition">
       <h2 className="text-xl font-semibold mb-2">{title}</h2>
-      <p className="text-gray-600">{body}</p>
-      <span className="text-xs text-gray-400 block mt-2">Post ID: {id}</span>
+      <p className="text-gray-700">{body}</p>
+      <div className="text-sm text-gray-500 mt-4">
+        <p>Post ID: {id}</p>
+        <p>User ID: {userId}</p>
+      </div>
     </div>
   );
 }
